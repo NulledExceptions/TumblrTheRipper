a3e0f9c5407f85a720e9365e9f41b9bdf5f3ae94
NulledExceptions <mmarnell1@gmail.com>
2016-10-25 16:49:30 -0500

    updating tagging in main, added sqls and further logging

--- main.py
+++ main.py
@@ -1,63 +1,77 @@
 #!/usr/bin/env python3
 from Scraper import Scraper
 from Parser import Parser
-import os
-from DBSQLLite import DBSQLLite
+from SQLLITE import SQLLITE
 import atexit
 import logging
 
 
-
-
 def printPercentage(start, total):
     percent = float(start) / float(total) * 100
-    print ("{0:.2f}".format(percent)+ "%  " + str(start) + " of total: " + str(total))
+    print("{0:.2f}".format(percent)+ "%  " + str(start) + " of " + str(total))
 
 
-def cleanUp(url, current,total):
+def blogExists():
+    carry_on= input('already exists. Would you like to continue anyways?(y/n)')
+    if (carry_on =='y'):
+        return True
+    elif (carry_on =='n'):
+        quit()
+    else:
+        blogExists()
+
+def cleanUp(records):
     logger = logging.getLogger(__name__)
-    records = (url, current, total)
+    logging.debug(records)
     logger.debug('Records: %s', records)
     logger.info('Updating records ...')
-    #u
-    #sql queries
-def main():
+    #SQLLITE().insert(records)
 
-    testurl= ''
-    '''
-    '''
+def main():
+    tagging = False
+    testurl= 'http://abc.tumblr.com/'
+
     # baseInput = sys.argv[1]
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     logger.info('Start blog: '+testurl)
 
     inputurl=Parser().formatInput(testurl)
-    print (inputurl)
-    dirname = Parser().getDirectoryName(testurl)
-    print (dirname)
+    logging.info(inputurl)
+    dirname = Parser().getDirectoryName(testurl,tagging)
+
+
+    logging.info(dirname)
     Parser().mkdirs(dirname)
-    os.chdir(dirname)
+    records = {}
+    records['total'] = Scraper().getTotalPosts(inputurl)
+    records['account'] = inputurl
+    records['current'] = 0
+    logger.info('Records: %s', records)
+    dbconn = SQLLITE()
+    if(dbconn.query_account(records['account'])):
+        blogExists()
+    #if not (SQLLITE().insert_initials(records)):
+    dbconn.insert(records)
 
-    total = Scraper().getTotalPosts(inputurl)
-    records = (inputurl, '0', total)
-    logger.debug('Records: %s', records)
-    logger.info('Updating records ...')
 
-    if(total and int(total)>0):
+    if(records['total'] and int(records['total'])>0):
         start=0
-        while( start <= int(total)):
-             printPercentage(start, total)
+        while( start <= int(records['total'])):
+             printPercentage(start, records['total'])
              if(start ==0):
                  url=inputurl
              else:
                 url= inputurl + '?start=' + str(start)
-             Scraper().scrapePage(url)
+             Scraper(tagging).scrapePage(url)
              start += 20
+             records['current'] = 0
 
-        records=(inputurl,start,total)
+        records['current']=start-20
+        #SQLLITE().insert(records)
         logger.debug('Records: %s', records)
         logger.info('Updating records ...')
-        atexit.register(cleanUp, inputurl, start, total)
+        atexit.register(cleanUp, records)
 
         # update records here
 
