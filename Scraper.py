842b711071e891bec0487fb96c088ac12e2fca5f
NulledExceptions <mmarnell1@gmail.com>
2016-10-25 16:38:30 -0500

    updating scraper file

--- Scraper.py
+++ Scraper.py
@@ -4,19 +4,18 @@ from Network import Network
 from Parser import Parser
 from xml.dom import minidom
 from xml.parsers.expat import ExpatError
-from DBSQLLite import DBSQLLite
+from SQLLITE import SQLLITE
+import logging
 
 
 
 
 class Scraper(object):
-    def __init__(self):
-        self.tagging = False
-        self.checklist = ('<tag>me</tag>', '<tag>me</tag>')
+    def __init__(self, tagging=False):
+        self.tagging = tagging
+        self.ignore_file_types=False
+        self.checklist = ('<tag>me</tag>','<tag>mine</tag>')
         self.ignore_format=['.gif','.png']
-
-
-
 
     def scrapePage(self, url):
         scraperIO = Parser()
@@ -30,7 +29,10 @@ class Scraper(object):
             xmldoc = minidom.parse(tumblrPage)
             postlist = xmldoc.getElementsByTagName('post')
         except ExpatError:
-            print ('unxexpected xml format')
+            logging.debug('unxexpected xml format')
+            pass
+        except AttributeError:
+            logging.debug('unxexpected ATTRIBUTE ERROR')
             pass
             # log
             # return flase
@@ -38,12 +40,13 @@ class Scraper(object):
             if((postlist or xmldoc)==False):
                 pass
             for eachpost in postlist:
-                #print(eachpost.attributes['type'].value)
+                #logging.debug(eachpost.attributes['type'].value)
                 if (self.tagging):
                     if not self.checkTags(eachpost):
                         continue
 
                 if eachpost.attributes['type'].value == 'photo':
+                    caption = self.checkCaption(eachpost)
                     urls =eachpost.getElementsByTagName('photo-url')
 
 
@@ -52,10 +55,21 @@ class Scraper(object):
                         if imageUrl:
                             imageFile=scraperNetwork.getURL(imageUrl)
                             if imageFile:
-                                #print(imageFile.headers.items())
+                                #logging.debug(imageFile.headers.items())
                                 imageFilename=scraperIO.formatImageName(imageUrl)
                                 if(imageFilename):
                                     scraperIO.writeFile(imageFilename,imageFile)
+                                    #logging.debug(eachpost.attributes.keys())
+                                    image={
+                                        'name':imageFilename,
+                                        'caption':caption,
+                                        'account':eachpost.attributes['id'].value,
+                                        'url':eachpost.attributes['url'].value,
+                                        'id': eachpost.attributes['id'].value,
+                                        'source':eachpost.attributes['url-with-slug'].value
+
+                                    }
+                                    SQLLITE().insertImage(image)
 
                 if eachpost.attributes['type'].value == 'video':
                     videoUrl= self.getVideoUrl(eachpost)
@@ -72,12 +86,12 @@ class Scraper(object):
             try:
                 xmldoc = minidom.parse(xmldoc)
             except ExpatError:
-                print('unxexpected xml format')
+                logging.debug('unxexpected xml format')
                 return False
                 #log
                 #return flase
             except OSError:
-                print('unxexpected os error ')
+                logging.debug('unxexpected os error ')
                 return False
 
 
@@ -87,7 +101,7 @@ class Scraper(object):
                     itemlist= xmldoc.getElementsByTagName('posts')
                     return itemlist[0].attributes['total'].value
                 except ExpatError:
-                    print("No posts or total, check url..")
+                    logging.debug("No posts or total, check url..")
                     #log
                     #return flase
                 return False
@@ -114,10 +128,10 @@ class Scraper(object):
                 vidsrc = vidsrc[:-4]
             return vidsrc
         except IndexError as e:
-            print('error')
-            print(e)
-            print(IndexError)
-            print(vidpost)
+            logging.debug('error')
+            logging.debug(e)
+            logging.debug(IndexError)
+            logging.debug(vidpost)
             pass
             #return flase , e
 
@@ -125,14 +139,20 @@ class Scraper(object):
         tags = eachpost.getElementsByTagName('tag')
         tags_found=False
         for tag in tags:
-            # print(tag.toxml())
+            # logging.debug(tag.toxml())
             if tag.toxml() in self.checklist:
                 tags_found=True
 
         if tags_found:
-            print("match found")
+            logging.info("match found")
             return True
         else:
             return False
 
+    def checkCaption(self,eachpost):
+        caption = eachpost.getElementsByTagName('photo-caption')
+        if(caption.length==0):
+            return None
+        else:
+            return caption[0].childNodes[0].nodeValue
 
